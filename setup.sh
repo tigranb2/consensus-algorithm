599a265c0695f2e3fca9198e5f81998662f5c95b
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-04-19 16:06:14 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,25 @@
+cd
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y build-essential linux-headers-$(uname -r)
+sudo apt install -y git zip python3-pip
+
+cd
+wget -q https://dl.google.com/go/go1.14.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
+rm go1.14.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /root/.bashrc
+echo 'export GOPATH=~/go' >> /root/.bashrc
+source /root/.bashrc
+
+cd
+mkdir mininet
+cd mininet
+git clone git://github.com/mininet/mininet
+cd mininet
+git checkout -b 2.2.2
+cd ..
+mininet/util/install.sh -a
+sudo mn --test pingall
+
+cd /usr/local/go/src/consensus-algorithm
