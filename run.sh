c22d9e5eb455179162606d027ff36268f34fc4ee
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-04-19 19:47:49 -0400

    modified chmod

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 if [[ -z $5 ]]; then
   echo "Please specify the algorithm, number of nodes, broadcast delay/timeout, loss rate, and description"
 else
-  chmod 700 consensus
+  chmod 700 $1
     for i in {1..5}; do
     python3 start.py $1 $2 $3 $4
     python3 analysis.py $5
