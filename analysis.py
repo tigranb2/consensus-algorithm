f38265c13fb72dc6dde6e56efc4f9123cfb43e21
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-04-22 22:49:32 -0400

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -11,16 +11,18 @@ def extract_data(f, string_to_search):
         if string_to_search in line:
             results.append(line)
 
-    return results[len(results)-1]
+    return results
 
 def main():
     f = read_file("performance.txt")
-    rates = extract_data(f, "Cumulative (sent/received/total):").split()
+    allRates = extract_data(f, "Cumulative (sent/received/total):")
+    rates = allRates[len(allRates)-2].split()
     tx = rates[2]
     rx = rates[3]
     
     f = read_file("log-1.txt")
-    time = extract_data(f, "Time taken:").split()[2]
+    allTimes = extract_data(f, "Time taken:")
+    time = allTimes[len(allTimes)-1].split()[2]
 
     data = time + " " + tx + " " + rx + "\n"
     file_name = f"{description}-data.txt"
